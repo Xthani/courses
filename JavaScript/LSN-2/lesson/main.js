e41c7763eb062cc311be47f9f858e85a98679669
7Xthani <7xthani@7Xthani.local>
2023-08-10 11:22:55 +0600

    home work

--- JavaScript/LSN-2/lesson/main.js
+++ JavaScript/LSN-2/lesson/main.js
@@ -33,11 +33,11 @@ const result = !!name ? name : 'Имени нет';
 
 // console.log(!name);
 
-console.log()
-console.log()
-console.log()
-console.log()
-console.log()
+console.log('main')
+console.log('main')
+console.log('main')
+console.log('main')
+console.log('main')
 
 
 
